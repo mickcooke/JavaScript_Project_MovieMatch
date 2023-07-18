4798593cf26fb74fbc287be95c387cbc8db3b036
ewanlaing <117400398+ewanlaing@users.noreply.github.com>
2023-07-18 11:30:04 +0100

    working on delete function

---
+++ client/src/movieService.js
@@ -0,0 +1,22 @@
+const baseURL = 'http://localhost:9000/movies/';
+
+export const getMovies = () => {
+  return fetch(baseURL)
+  .then(response => response.json());
+}
+
+export const postMovie= (movie) => {
+  return fetch(baseURL, {
+    method:'POST',
+    body: JSON.stringify(movie),
+    headers: {'Content-Type': 'application/json'}
+  })
+  .then(response => response.json());
+}
+
+export const deleteMovie= (id) => {
+    return fetch(baseURL + id, {
+        method: 'DELETE'
+    })
+  
+}
