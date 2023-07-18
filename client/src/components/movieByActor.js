2acec491875a946f09cd1eba1caec8c93f7fe63c
mickcooke <128602534+mickcooke@users.noreply.github.com>
2023-07-18 15:11:43 +0100

    added FavouritesHeart t o movieByActor

--- client/src/components/movieByActor.js
+++ client/src/components/movieByActor.js
@@ -3,10 +3,6 @@ import FavouritesHeart from "./favouritesHeart";
 
 const MovieByActor = ({movie, toggleFavourites}) => {
 
-  const handleClick = () => {
-    console.log(movie)
-    toggleFavourites(movie)
-  }
 
   return (
     <>
@@ -15,9 +11,7 @@ const MovieByActor = ({movie, toggleFavourites}) => {
       <div className='description-box'>
           <Link to={`/movie/${movie.imdbID}`}><h3>{movie.Title}</h3></Link>
       </div>
-      <div className='zoom'>
-          <img onClick={handleClick} src={require("../images/Heart1.png")}/>
-      </div>
+      <FavouritesHeart movie={movie} toggleFavourites={toggleFavourites}/>
     </div>
     </>
   )
