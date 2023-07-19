0f4f97da34d7789e29685bef37ca3b1d142f4f17
mickcooke <128602534+mickcooke@users.noreply.github.com>
2023-07-19 22:41:57 +0100

    filling hearts complete

--- client/src/components/header.js
+++ client/src/components/header.js
@@ -4,22 +4,58 @@ import {Link} from 'react-router-dom';
 
 const Header = ({favouriteMovies}) => {
 
+const oneFavs = () => {
+  return(favouriteMovies.length >= 1)
+}
+
+const threeFavs = () => {
+  return(favouriteMovies.length >= 3)
+}
+
 const fiveFavs = () => {
   return(favouriteMovies.length >= 5)
 }
 
-const tenFavs = () => {
-  return(favouriteMovies.length >= 10)
+const sevenFavs = () => {
+  return(favouriteMovies.length >= 7)
+}
+
+const nineFavs = () => {
+  return(favouriteMovies.length >= 9)
+}
+
+const elevenFavs = () => {
+  return(favouriteMovies.length >= 11)
+}
+
+const thirteenFavs = () => {
+  return(favouriteMovies.length >= 13)
+}
+
+const fifteenFavs = () => {
+  return(favouriteMovies.length >= 15)
 }
 
   return(
     <div className="Header">
       <div className="heart-container">
         <div className="heart">
-          {tenFavs()
+          {fifteenFavs()
            ? <Link to={'/favourites'}><img src={require("../images/HeartSolidWhite.png")}/></Link>
+           : thirteenFavs()
+           ? <Link to={'/favourites'}><img src={require("../images/HeartAtThirteen.png")}/></Link>
+           : elevenFavs()
+           ? <Link to={'/favourites'}><img src={require("../images/HeartAtEleven.png")}/></Link>
+           : nineFavs()
+           ? <Link to={'/favourites'}><img src={require("../images/HeartAtNine.png")}/></Link>
+           : sevenFavs()
+           ? <Link to={'/favourites'}><img src={require("../images/HeartAtSeven.png")}/></Link>
            : fiveFavs()
            ? <Link to={'/favourites'}><img src={require("../images/HeartHalfFull.png")}/></Link>
+           : threeFavs()
+           ? <Link to={'/favourites'}><img src={require("../images/HeartAtThree.png")}/></Link>
+           : oneFavs()
+           ? <Link to={'/favourites'}><img src={require("../images/HeartAtOne.png")}/></Link>
           : <Link to={'/favourites'}><img src={require("../images/Heart1.png")}/></Link>
           }
       </div>
