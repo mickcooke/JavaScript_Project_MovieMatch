6b2f9031689a26213fea015447b7a4c33dc240b7
mickcooke <128602534+mickcooke@users.noreply.github.com>
2023-07-19 17:15:45 +0100

    styled favourites

--- client/src/components/favouriteMovie.js
+++ client/src/components/favouriteMovie.js
@@ -6,15 +6,21 @@ const FavouriteMovie = ({movie, toggleFavourites}) => {
 
     return(
       <>
-      <div className='Item-box'>
-      <Link to={`/movie/${movie.imdbID}`}><img src={movie.Poster} className='img'/></Link>
+    <div className='Item-box-2'>
+      <Link to={`/movie/${movie.imdbID}`}><img src={movie.Poster} className='img' alt="poster"/></Link>
       <div className='description-box'>
-      <Link to={`/movie/${movie.imdbID}`}><h3>{movie.Title}</h3></Link>
-      <FavouritesHeart movie={movie} toggleFavourites={toggleFavourites}/>
+        <div className='heart-and-title'>
+          <div className="heart-lower">
+            <FavouritesHeart movie={movie} toggleFavourites={toggleFavourites}/>
+          </div>
+          <div>
+            <Link to={`/movie/${movie.imdbID}`}><h3 className='white-text'>{movie.Title}</h3></Link>
+          </div>
+        </div>
+        <p className='text-raise'>Dir: {movie.Director}</p>
       </div>
-      </div>
-  
-      </>
+    </div>
+    </>
     )
   }
 
